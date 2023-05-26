6110987e88bdca5e63e46d592eba4593cd515ac5
ShankarSubramanyam055 <shankarsubramanyam055@gmail.com>
2023-05-26 12:37:55 +0530

    comment removed

--- php/upload.php
+++ php/upload.php
@@ -5,4 +5,3 @@
     $file_up_name = time().$file_name; //making file name dynamic by adding time before file name
     move_uploaded_file($tmp_name, "files/".$file_up_name); //moving file to the specified folder with dynamic name
 ?>
-<!-- PHP ends here -->
